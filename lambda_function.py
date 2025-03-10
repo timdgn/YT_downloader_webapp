611d66e964b042101d33bdeaab45658c4eaa0da7
TimDgn <timmothy86264020@gmail.com>
2025-03-10 02:27:29 +0100

    [Refactor] Better BOT_TOKEN implementation

--- lambda_function.py
+++ lambda_function.py
@@ -31,15 +31,15 @@ def get_secret_bot_token():
 
     return secret['bot_token']
 
-def send_message(bot_token, chat_id, message):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+def send_message(chat_id, message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     encoded_data = json.dumps(data).encode('utf-8')
     http.request('POST', url, body=encoded_data, headers={'Content-Type': 'application/json'})
 
-def send_video(bot_token, chat_id, file_path):
+def send_video(chat_id, file_path):
     file_name = os.path.basename(file_path)
-    url = f"https://api.telegram.org/bot{bot_token}/sendVideo"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendVideo"
 
     with open(file_path, 'rb') as video:
         video_data = video.read()  # Lire tout le fichier en bytes
@@ -94,14 +94,14 @@ def lambda_handler(event, context):
     message_text = body['message']['text']
 
     # Get the Telegram bot token from Secrets Manager
-    bot_token = get_secret_bot_token()
-    print(f"*** Bot Token : {bot_token}")
+    global BOT_TOKEN
+    BOT_TOKEN = get_secret_bot_token()
 
     # Assert there is 2 parts
     parts = message_text.split()
     if len(parts) < 2:
         msg = "Please send the URL followed by the quality (low, medium, high, very high). Example: https://youtu.be/example high"
-        send_message(bot_token, chat_id, msg)
+        send_message(chat_id, msg)
         return {'statusCode': 200, 'body': json.dumps('Invalid input')}
     url, quality = parts[0], " ".join(parts[1:])
     print(f"*** URL : {url}")
@@ -111,18 +111,18 @@ def lambda_handler(event, context):
     allowed_qualities = ["low", "medium", "high", "very high"]
     if quality not in allowed_qualities:
         msg = "Invalid quality. Please choose from low, medium, high, or very high."
-        send_message(bot_token, chat_id, msg)
+        send_message(chat_id, msg)
         print(f"*** Invalid quality : {quality}")
         return {'statusCode': 200, 'body': json.dumps('Invalid quality')}
 
     # Download the video
-    send_message(bot_token, chat_id, "Downloading video, please wait...")
+    send_message(chat_id, "Downloading video, please wait...")
     file_path = download_video(url, quality)
     print(f"*** File Path : {file_path}")
     if file_path:
-        send_video(bot_token, chat_id, file_path)
+        send_video(chat_id, file_path)
         os.remove(file_path)  # Clean up after sending
     else:
-        send_message(bot_token, chat_id, "Failed to download the video. Please try again.")
+        send_message(chat_id, "Failed to download the video. Please try again.")
     
     return {'statusCode': 200, 'body': json.dumps('Message processed successfully')}
